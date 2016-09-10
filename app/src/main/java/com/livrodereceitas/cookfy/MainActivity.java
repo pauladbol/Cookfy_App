992d09041bfe548f61792946a12ee2cd135ffd57
Paula Bol <pauladbol@hotmail.com>
2016-09-09 21:08:37 -0300

    Iniciando layout das telas iniciais

--- app/src/main/java/com/livrodereceitas/cookfy/MainActivity.java
+++ app/src/main/java/com/livrodereceitas/cookfy/MainActivity.java
@@ -1,7 +1,10 @@
 package com.livrodereceitas.cookfy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button login = (Button) findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intentLoga = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intentLoga);
+            }
+        });
+
+        Button cadastrar = (Button) findViewById(R.id.cadastrar);
+
+        cadastrar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intentCadastra = new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(intentCadastra);
+            }
+        });
     }
 
     protected String clicou() {
