b7571e2775d86730dade3437e9ca0fb9a6d50474
pauladbol <pauladbol@hotmail.com>
2016-10-02 17:43:20 -0300

    Conexoes com o banco, hash e token

---
+++ app/src/main/java/com/livrodereceitas/cookfy/DetalheActivity.java
@@ -0,0 +1,20 @@
+package com.livrodereceitas.cookfy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class DetalheActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhe);
+
+//        String[] ingredientes = {"Ingrediente1", "Ingrediente2", "Ingrediente3"};
+//        ListView lista_ingredientes = (ListView) findViewById(R.id.lista_ingredientes);
+//        ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_2, ingredientes);
+//        lista_ingredientes.setAdapter(adapter);
+    }
+}
