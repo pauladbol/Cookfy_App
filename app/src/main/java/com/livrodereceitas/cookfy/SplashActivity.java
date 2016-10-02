98fc58ca3900a5c91afa0d0bc772ceec97395d00
pauladbol <pauladbol@hotmail.com>
2016-10-01 23:14:42 -0300

    Splash screen e novo icone

--- app/src/main/java/com/livrodereceitas/cookfy/SplashActivity.java
+++ app/src/main/java/com/livrodereceitas/cookfy/SplashActivity.java
@@ -5,11 +5,27 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by pauladbol on 2016-09-29.
  */
 public class SplashActivity extends Activity {
+    private static final String REGISTER_URL = "http://simplifiedcoding.16mb.com/UserRegistration/volleyRegister.php";
+    public static final String KEY_TOKEN = "token";
+
     // Timer da splash screen
     private static int SPLASH_TIME_OUT = 3000;
 
@@ -24,14 +40,55 @@ public class SplashActivity extends Activity {
              */
             @Override
             public void run() {
+                verifyUser();
+
+                final boolean token = true;
+
+                if (token) {
+                    Intent i = new Intent(SplashActivity.this, Main2Activity.class);
+                    startActivity(i);
+                } else {
+                    Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(i);
+                }
                 // Esse método será executado sempre que o timer acabar
                 // E inicia a activity principal
-                Intent i = new Intent(SplashActivity.this, Main2Activity.class);
-                startActivity(i);
+
 
                 // Fecha esta activity
                 finish();
             }
+
+
         }, SPLASH_TIME_OUT);
+    }
+
+    private void verifyUser(){
+        final String token = "token";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, REGISTER_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(SplashActivity.this,response,Toast.LENGTH_LONG).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(SplashActivity.this,error.toString(),Toast.LENGTH_LONG).show();
+                    }
+                }){
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<String, String>();
+                params.put(KEY_TOKEN,token);
+                return params;
+            }
+
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 }
