a142e0e8aa8e3102f652452fb63e5ebdc20977d4
pauladbol <pauladbol@hotmail.com>
2016-10-27 23:00:48 -0200

    Ajustando menu lateral

--- app/src/main/java/com/livrodereceitas/cookfy/Activities/DrawerActivity.java
+++ app/src/main/java/com/livrodereceitas/cookfy/Activities/DrawerActivity.java
@@ -1,18 +1,18 @@
 package com.livrodereceitas.cookfy.Activities;
 
 import android.content.Intent;
-import android.support.v4.view.GravityCompat;
+import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.view.MotionEvent;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.MotionEvent;
-import android.view.View;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -27,7 +27,8 @@ import com.livrodereceitas.cookfy.R;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class Main2Activity extends AppCompatActivity {
+public class DrawerActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final String REGISTER_URL = "https://cookfy.herokuapp.com/recipes/";
     public static final String KEY_USERNAME = "user";
@@ -48,7 +49,27 @@ public class Main2Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_drawer);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ViewPager g = (ViewPager) findViewById(R.id.viewPager);
         g.setAdapter(new ImagemPagerAdapter(this, imagens));
@@ -81,14 +102,77 @@ public class Main2Activity extends AppCompatActivity {
 
                     @Override
                     public void onClick(View view) {
-                        Intent intentLogar = new Intent(Main2Activity.this, ListaReceitasActivity.class);
+                        Intent intentLogar = new Intent(DrawerActivity.this, ListaReceitasActivity.class);
                         startActivity(intentLogar);
 
                     }
                 }
         );
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.drawer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
 
 
+        if (id == R.id.nav_perfil) {
+            // Handle the camera action
+            Intent intentPerfil = new Intent(DrawerActivity.this, DetalheActivity.class);
+            startActivity(intentPerfil);
+        } else if (id == R.id.nav_favoritos) {
+            Intent intentFav = new Intent(DrawerActivity.this, ListaReceitasActivity.class);
+            startActivity(intentFav);
+
+        } else if (id == R.id.nav_config) {
+            Intent intentConfig = new Intent(DrawerActivity.this, ListaReceitasActivity.class);
+            startActivity(intentConfig);
+
+        } else if (id == R.id.nav_sair) {
+            Intent intentSair = new Intent(DrawerActivity.this, ListaReceitasActivity.class);
+            startActivity(intentSair);
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+
+
+
+        return true;
     }
 
     private void pegaReceitas(){
@@ -107,15 +191,15 @@ public class Main2Activity extends AppCompatActivity {
                     ingredientes = response.getString("ingredientes");
                     recipeBooks = response.getString("recipeBooks");
 
-                    Toast.makeText(Main2Activity.this, "Bem vindo!", Toast.LENGTH_LONG).show();
-                    Intent intentLogar = new Intent(Main2Activity.this, ListaReceitasActivity.class);
+                    Toast.makeText(DrawerActivity.this, "Bem vindo!", Toast.LENGTH_LONG).show();
+                    Intent intentLogar = new Intent(DrawerActivity.this, ListaReceitasActivity.class);
                     startActivity(intentLogar);
                     finish();
 
                 } catch (JSONException e) {
                     e.printStackTrace();
                     Toast.makeText(getApplicationContext(),"Error: " + e.getMessage(),Toast.LENGTH_LONG).show();
-                    Intent intentLogar = new Intent(Main2Activity.this, Main2Activity.class);
+                    Intent intentLogar = new Intent(DrawerActivity.this, DrawerActivity.class);
                     startActivity(intentLogar);
                     finish();
                 }
@@ -124,7 +208,7 @@ public class Main2Activity extends AppCompatActivity {
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(Main2Activity.this,error.toString(),Toast.LENGTH_LONG).show();
+                        Toast.makeText(DrawerActivity.this,error.toString(),Toast.LENGTH_LONG).show();
                     }
                 }){
 
@@ -134,7 +218,4 @@ public class Main2Activity extends AppCompatActivity {
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         requestQueue.add(jsonObjReq);
     }
-
 }
-
-
