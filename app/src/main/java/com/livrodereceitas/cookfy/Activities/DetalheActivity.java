6aeee13f185d2b735dd306f96d2bf7ab60f106ab
Mazuhim <thiagomazuhim@gmail.com>
2016-10-29 00:40:01 -0200

    favorito

--- app/src/main/java/com/livrodereceitas/cookfy/Activities/DetalheActivity.java
+++ app/src/main/java/com/livrodereceitas/cookfy/Activities/DetalheActivity.java
@@ -3,6 +3,9 @@ package com.livrodereceitas.cookfy.Activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 import com.livrodereceitas.cookfy.Helpers.DetalheHelper;
 import com.livrodereceitas.cookfy.R;
@@ -16,9 +19,21 @@ public class  DetalheActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detalhe);
         helper = new DetalheHelper(this);
 
+        final CheckBox favorito = (CheckBox) findViewById(R.id.favorita);
         Intent intent = getIntent();
         Recipes receita = (Recipes) intent.getSerializableExtra("receita");
 
+
+        favorito.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(favorito.isChecked()){
+                    Toast.makeText(DetalheActivity.this, "agora é favorito " + favorito.isChecked() , Toast.LENGTH_SHORT).show();
+                }
+                else
+                    Toast.makeText(DetalheActivity.this, "não é mais favorito " + favorito.isChecked(), Toast.LENGTH_SHORT).show();
+            }
+        });
         //helper.preencheDetalhe(receita);
 
 //        String[] ingredientes = {"Ingrediente1", "Ingrediente2", "Ingrediente3"};
