ac391a92c2c2cb37a2df5f4f7c34f7224dfe4e82
pauladbol <pauladbol@hotmail.com>
2016-10-29 01:28:01 -0200

    Finalizando menu lateral na tela inicial e iniciando perfil de usuario

---
+++ app/src/main/java/com/livrodereceitas/cookfy/Activities/PerfilActivity.java
@@ -0,0 +1,15 @@
+package com.livrodereceitas.cookfy.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.livrodereceitas.cookfy.R;
+
+public class PerfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil);
+    }
+}
